2a00b97cbb16e27f9757f100211b2abcdd090f37
erica <1054447104@qq.com>
2018-01-17 10:07:15 +0800

    erica

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+    	<div class="tab-item">
+    	  <router-link to="/goods">goods</router-link>
+    	</div>
+    	<div class="tab-item">
+        <router-link to="/ratings">ratings</router-link>
+    	</div>
+    	<div class="tab-item">
+        <router-link to="/seller">seller</router-link>
+    	</div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+	import header from './components/header/header.vue';
+
+	export default {
+	  components: {
+	    'v-header': header
+	  }
+	};
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+// @import 是stylus的特殊语法 可以引入如何路径下的styl
+  @import"./common/stylus/mixin.styl";
+
+// 使用伪类after来实现边线1像素；直接使用border-bottom在手机上看是比较粗的，因为如ipone6的dpi是2，所以线条为2像素
+// &表示他的父元素 即.tab-item；> 为后排选择器
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
+</style>
---
+++ src/main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App';
+import goods from '@components/goods/goods';
+import seller from '@components/seller/seller';
+import ratings from '@components/ratings/ratings';
+
+import '@common/stylus/index.styl';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+// 定义路由(配置路由)
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/seller', component: seller},
+  {path: '/ratings', component: ratings}
+];
+
+// 创建一个路由实例
+const router = new VueRouter({
+  // 设置选中的路由的颜色
+  linkActiveClass: 'active',
+  routes
+});
+
+// 在js中使用new来创建一个实例对象是要赋给某个值的，但这里不需要，所以在代码上单独配一个规则，跳过该校验规则（no-new是规则名）
+/* eslint-disable no-new */
+new Vue({
+  router,
+  components: {App}
+}).$mount('#app');
+
+router.push('/goods');
