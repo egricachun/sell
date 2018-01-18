c59f10bca26bcef55dfa86dcecc04ad2f42c4177
erica <1054447104@qq.com>
2018-01-18 18:23:01 +0800

    erica

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -15,8 +15,18 @@
   	  	<span class="text" v-text="seller.supports[0].description"></span>
   	  </div>
   	</div>
+  	<div class="support-count" v-if="seller.supports">
+  		<span class="count">{{seller.supports.length}}个</span>
+  		<i class="icon-keyboard_arrow_right"></i>
+  	</div>
   </div>
-  <div class="bulletin-wrapper"></div>
+  <div class="bulletin-wrapper">
+  	<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+  	<i class="icon-keyboard_arrow_right"></i>
+  </div>
+  <div class="background">
+  	<img :src="seller.avatar" width="100%" height="100%">
+  </div>
 </div>
 </template>
 
@@ -36,18 +46,16 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
 // 使用display：inline-block时 中间会有缝隙（空白字符），可在父元素中设置font-size:0
 // css中只能写相对路径，不能写@common
+// font-size:0会影响省略号的显示
   @import "../../common/stylus/mixin"
   .header
-  	color: #fff;
-  	background-color: #000;
+  	position: relative
+  	color: #fff
+  	background-color: rgba(7, 17, 27, 0.5)
   	.content-wrapper
+  		position: relative
   		padding: 24px 12px 18px 24px
   		font-size: 0
-  		.avatar
-  			display: inline-block
-  			vertical-align: top
-  			img
-  				border-radius: 2px
   		.content
   			display: inline-block
   			margin-left: 16px
@@ -91,5 +99,63 @@ export default {
   						bg-image('guarantee_1')
 				  .text
             line-height: 12px
-            font-size: 10px        
+            font-size: 10px
+  		.support-count
+      	position: absolute
+      	position: absolute
+      	right: 12px
+      	bottom: 14px
+      	padding: 0 8px
+      	height: 24px
+      	line-height: 24px
+      	border-radius: 14px
+      	background-color: rgba(0, 0, 0, 0.2)
+      	text-align: center
+      	.count
+      		vertical-align: top
+      		font-size: 10px
+      	.icon-keyboard_arrow_right
+      		margin-left: 2px
+      		font-size: 10px
+      		line-height: 24px
+  		.avatar
+  			display: inline-block
+  			vertical-align: top
+  			img
+  				border-radius: 2px
+  	.bulletin-wrapper
+    	position: relative
+    	height: 28px
+    	line-height: 28px
+    	padding: 0 22px 0 12px
+    	white-space: nowrap
+    	overflow: hidden
+    	text-overflow: ellipsis
+    	background: rgba(7, 17, 27, 0.2)
+    	.bulletin-title
+    		display: inline-block
+    		vertical-align: top
+    		margin-top: 7px
+    		width: 22px
+    		height: 12px
+    		bg-image('bulletin')
+    		background-size: 22px 12px
+    		background-repeat: no-repeat
+    	.bulletin-text
+    		vertical-align: top
+    		margin: 0 4px
+    		font-size: 10px
+    	.icon-keyboard_arrow_right
+    		position: absolute
+    		font-size: 10px
+    		right: 12px
+    		top: 8px
+  	.background
+  		position: absolute
+  		top: 0
+  		left: 0
+  		width: 100%
+  		height: 100%
+  		z-index: -1
+  		filter: blur(10px)// 产生模糊效果
 </style>
