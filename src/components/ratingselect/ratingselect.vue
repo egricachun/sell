072f2c0939029259672f7c1289d040f6ddb4b2aa
erica <1054447104@qq.com>
2018-01-25 00:51:57 +0800

    阿迪斯
    Signed-off-by: erica <1054447104@qq.com>

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="ratingselect">
   	<div class="rating-type border-1px">
-  		<span class="block positive" :class="{'active': selectType === 2}">{{desc.all}}<span class="count">22</span></span>
-  		<span class="block positive" :class="{'active': selectType === 0}">{{desc.positive}}<span class="count">25</span></span>
-  		<span class="block negative" :class="{'active': selectType === 1}">{{desc.negative}}<span class="count">32</span></span>
+  		<span @click="select(2, $event)" class="block positive" :class="{'active': selectType === 2}">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+  		<span @click="select(0, $event)" class="block positive" :class="{'active': selectType === 0}">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+  		<span @click="select(1, $event)" class="block negative" :class="{'active': selectType === 1}">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
   	</div>
-  	<div class="switch">
-  	  <i></i>
+  	<div @click="toggleContent($event)" class="switch" :class="{'on':onlyContent}">
+  	  <span class="icon-check_circle"></span>
   		<span class="text">只看有内容的评价</span>
   	</div>
   </div>
 </template>
 <script type="text/ecmascript-6">
-  // const POSITIVE = 0;
-  // const NEGATIVE = 1;
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
   const ALL = 2;
 
   export default {
     props: {
       // 评价信息
-      rating: {
+      ratings: {
         type: Array,
         default() {
           return [];
@@ -45,6 +45,37 @@
             negative: '不满意'
           };
         }
+      }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE;
+        });
+      },
+      negatives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE;
+        });
+      }
+    },
+    methods: {
+      // 选择评价类型 
+      select(type, event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.selectType = type;
+        // 通知父组件参数的变化
+        this.$emit('ratingtypeSelect', type);
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.onlyContent = !this.onlyContent;
+        // 通知父组件参数的变化
+        this.$emit('contentToggle', this.onlyContent);
       }
     }
   };
@@ -80,4 +111,23 @@
           background-color: rgba(77, 85, 93, 0.2)
           &.active
             background-color: rgb(77, 85, 93)
+    .switch
+      padding: 12px 18px
+      line-height: 24px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      &.on
+        .icon-check_circle
+          color: #00c850
+      // 图标和文字都设置display和vertical-align 可实现垂直居中对齐
+      .icon-check_circle
+        display: inline-block
+        vertical-align: top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
 </style>
